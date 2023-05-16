a0fee736720625b7b079a9a8d6a551831b5df789
Eligio Biasi <ex_98@live.it>
2023-05-16 16:50:12 +0200

    i cannot

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-Consegna:
-// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-// Come faccio a sapere se un numero è divisibile per un altro?
-// Abbiamo visto qualcosa di particolare che possiamo usare?
+const elemenUl = document.querySelector ('ul');
 
 for (let index = 1; index < 101; index++){
+    
     if ((index % 3 === 0) && (index % 5 === 0)) {
         console.log('BuzzFizz');
     } else if (index % 5 === 0){
@@ -15,4 +12,7 @@ for (let index = 1; index < 101; index++){
     } else{
         console.log(index)
     }
+    const li = document.createElement('li');
+    li.innerHTML = (index);
+    elemenUl.append(li)
 }
